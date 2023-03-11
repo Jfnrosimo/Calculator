1204e9c2591c53adf4d5d1dfd12129935affcb46
Jfnrosimo <j.rosimo098.com>
2023-03-11 10:56:38 +0800

    Fix: button positions

--- CalcuAppWpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ CalcuAppWpf/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -189,7 +189,7 @@ namespace CalcuAppWpf {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CalcuAppWpf;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CalcuAppWpf;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
